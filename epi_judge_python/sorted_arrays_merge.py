4506d120d1159a69f8f516c633e2e71a77384b3b
Jesse Tham <jtham@ualberta.ca>
2018-09-27 21:53:52 -0600

    Solutions

--- epi_judge_python/sorted_arrays_merge.py
+++ epi_judge_python/sorted_arrays_merge.py
@@ -2,8 +2,21 @@ from test_framework import generic_test
 
 
 def merge_sorted_arrays(sorted_arrays):
-    # TODO - you fill in here.
-    return []
+    import heapq
+    from collections import namedtuple
+    result = []
+    min_heap = []
+    ArrayValue = namedtuple('ArrayValue', ['value', 'array', 'position'])
+    for i, array in enumerate(sorted_arrays):
+        heapq.heappush(min_heap, ArrayValue(array[0], i, 0))
+
+    while min_heap:
+        value = heapq.heappop(min_heap)
+        result.append(value.value)
+        if value.position < len(sorted_arrays[value.array]) - 1:
+            array = sorted_arrays[value.array]
+            heapq.heappush(min_heap, ArrayValue(array[value.position+1], value.array, value.position + 1))
+    return result
 
 
 if __name__ == '__main__':
